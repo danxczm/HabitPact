a02bd49f83bd68677415f847aa231ad7ee8cb489
danbohdan <81313913+danxczm@users.noreply.github.com>
2025-11-08 19:05:03 +0200

    Fix: ignore .next directory properly

--- .next/dev/server/chunks/ssr/Documents_personal_HabitPact__next-internal_server_app_page_actions_ebf9bf4d.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Documents/personal/HabitPact/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=Documents_personal_HabitPact__next-internal_server_app_page_actions_ebf9bf4d.js.map
