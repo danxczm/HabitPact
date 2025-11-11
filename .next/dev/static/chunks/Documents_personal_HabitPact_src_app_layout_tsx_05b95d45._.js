1e6ce26a73db7a23de91ef2a66bab698a65aaa34
danbohdan <81313913+danxczm@users.noreply.github.com>
2025-11-11 19:05:45 +0200

    Remove .next from tracking

--- .next/dev/static/chunks/Documents_personal_HabitPact_src_app_layout_tsx_05b95d45._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Documents_personal_HabitPact_src_styles_globals_scss_57fec936.css"
-],
-    source: "dynamic"
-});
